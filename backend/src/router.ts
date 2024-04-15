abf42903d880015f1685561ee0b2e6c5f86eaa67
gabicilfone <cilfonegabriel@gmail.com>
2024-04-15 01:15:35 -0300

    Creación de rutas

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { pokemonListRouter } from "./handlers/pokemonsRoute";
+import { getPokemonById } from "./handlers/pokemonDetails";
 
 const router = Router();
 
-router.get('/api/pokemons', pokemonListRouter)  
+router.get('/api/pokemon', pokemonListRouter)  
+router.get('/api/pokemon/:id', getPokemonById);
 
 export default router;
