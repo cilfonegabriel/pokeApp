bc649d223ea2b924d2b24ad5df940161ddddfcc0
gabicilfone <cilfonegabriel@gmail.com>
2024-04-17 19:01:34 -0300

    Añadimos el router de app dentro del provider

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import AppRouter from "./AppRouter"
+import {PokemonProvider} from "./context/PokemonProvider"
+
+export default function App() {
+  return (
+    <PokemonProvider>
+      <AppRouter />
+    </PokemonProvider>
+  )
+}
