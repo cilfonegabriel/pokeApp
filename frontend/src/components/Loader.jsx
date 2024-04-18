01781d53d782a29a47b1b5776f8ffec405dfdd37
gabicilfone <cilfonegabriel@gmail.com>
2024-04-18 09:45:50 -0300

    Agregamos el ícono de cargando

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,11 +1,17 @@
+import 'ldrs/dotSpinner'
 
 const Loader = () => {
   return (
-    <div>
-      Desde loader
-
+    <div className="flex justify-center mt-200">
+      <l-dot-spinner
+        size="40"
+        speed="0.9"
+        color="black" 
+      ></l-dot-spinner>
     </div>
+
+
   )
 }
 
-export default Loader
+export default Loader;
